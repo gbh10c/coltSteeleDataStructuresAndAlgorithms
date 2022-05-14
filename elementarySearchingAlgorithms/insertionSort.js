585500cb767ded13b44c5eaf00f26122bcc0efba
gbh10c <grandcasa10@gmail.com>
2022-05-14 00:32:47 -0400

    Insertion Sort example

---
+++ elementarySearchingAlgorithms/insertionSort.js
@@ -0,0 +1,14 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+      arr[j] = currentVal;
+      console.log(arr);
+    }
+  }
+  return arr;
+}
+
+// insertionSort([2, 1, 9, 76, 4]);
+// insertionSort([12, 11, 15, 14, 13]);
